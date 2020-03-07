71f7d8816a424587baeb0cb6def7ff424f36e0a0
Karlatemp <karlatemp@vip.qq.com>
2020-03-07 21:40:56 +0800

    Update Check

--- src/HamsterYDS/UntilTheEnd/UntilTheEnd.java
+++ src/HamsterYDS/UntilTheEnd/UntilTheEnd.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -32,90 +33,113 @@ import me.clip.placeholderapi.metrics.bukkit.Metrics;
  * @author 南外丶仓鼠
  * @version V5.1.1
  */
-public class UntilTheEnd extends JavaPlugin implements Listener{
-	String latestVersion;
-	boolean isLatest=true;
-	@Override public void onEnable() {
-		Metrics metrics = new Metrics(this);
-		metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
-		Bukkit.getPluginManager().registerEvents(this,this);
-		checkUpdate();
-		loadConfig();
-		new Config(this);
-		new World(this);
-		new Temperature(this);
-		new Sanity(this);
-		new Humidity(this);
-	  //new Tiredness(this);
-		new Guide(this);
-		new Crops(this);
-		new HamsterYDS.UntilTheEnd.player.Player(this);
-		new HudProvider(this);
-		new Food(this);
-		new Block(this);
-		new ItemManager(this);
-		new Commands(this);
-	}
-	@Override public void onDisable() {
-		for(Player player:Bukkit.getOnlinePlayers()) {
-			PlayerManager.save(player.getName());
-		}
-		WorldProvider.saveWorlds();
-		BlockManager.saveBlocks();
-	}
-	public void loadConfig() {
-		File file=new File(this.getDataFolder(),"config.yml");
-		if(!file.exists())
-			saveResource("config.yml",true);
-		String language=getConfig().getString("language");
-		File langFile=new File(this.getDataFolder(),language);
-		if(!langFile.exists())	
-			saveResource(language, false);
-	}
-	@EventHandler public void onJoin(PlayerJoinEvent event) {
-		if(!isLatest)
-			event.getPlayer().sendMessage("§6§l[UntilTheEnd]§4§l您的插件不是最新版，很有可能有BUG！");
-	}
-	public void checkUpdate() {
-		int pluginId=6586;
-        Metrics metrics=new Metrics(this);
-		Bukkit.getConsoleSender().sendMessage("[UntilTheEnd]插件检查更新中......");
-		System.out.println("[UntilTheEnd]插件当前版本为>>"+getDescription().getVersion());
-		latestVersion=getLatestVersion();
-		System.out.println(latestVersion);
-		new BukkitRunnable() {
-			public void run() {
-				if(latestVersion.equalsIgnoreCase(getDescription().getVersion())) {
-					getLogger().info("您的插件已经是最新版啦！");
-				}else {
-					getLogger().info("您的插件不是最新版，请立即更新！");
-					isLatest=false;
-				}
-			}
-		}.runTaskAsynchronously(this);
-	}
-	public static String getLatestVersion() {
-		HttpURLConnection connection = null;
-		try {
-			int timeout=5000;
-			URL url=new URL("https://untiltheend.coding.net/p/UntilTheEnd/d/UntilTheEnd/git/raw/master/UTEversion.txt");
-		    connection=(HttpURLConnection)url.openConnection();
-		    connection.setConnectTimeout(timeout);
-		    InputStream inStream=connection.getInputStream();
-		    final StringBuilder builder=new StringBuilder(255);
-		    int byteRead;
-		    while ((byteRead=inStream.read())!=-1) {
-		    	builder.append((char) byteRead);
-		    }
-		    String response=builder.toString();
-		    return response;
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		} finally {
-			if (connection!=null) {
-				connection.disconnect();
-			}
-		}
-		  return "无法获取"; //这个return代表获取不到版本时返回的字符串，当然你可以改别的
-	}
+public class UntilTheEnd extends JavaPlugin implements Listener {
+    String latestVersion;
+    boolean isLatest = true;
+    private static UntilTheEnd INSTANCE;
+
+    public static UntilTheEnd getInstance() {
+        return INSTANCE;
+    }
+
+    public UntilTheEnd() {
+        INSTANCE = this;
+    }
+
+    @Override
+    public void onEnable() {
+        int pluginId = 6586;
+        Metrics metrics = new Metrics(this);
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
+        checkUpdate();
+        loadConfig();
+        new Config(this);
+        new World(this);
+        new Temperature(this);
+        new Sanity(this);
+        new Humidity(this);
+        //new Tiredness(this);
+        new Guide(this);
+        new Crops(this);
+        new HamsterYDS.UntilTheEnd.player.Player(this);
+        new HudProvider(this);
+        new Food(this);
+        new Block(this);
+        new ItemManager(this);
+        new Commands(this);
+    }
+
+    @Override
+    public void onDisable() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            PlayerManager.save(player.getName());
+        }
+        WorldProvider.saveWorlds();
+        BlockManager.saveBlocks();
+    }
+
+    public void loadConfig() {
+        File file = new File(this.getDataFolder(), "config.yml");
+        if (!file.exists())
+            saveResource("config.yml", true);
+        String language = getConfig().getString("language");
+        File langFile = new File(this.getDataFolder(), language);
+        if (!langFile.exists())
+            saveResource(language, false);
+    }
+
+    public void checkUpdate() {
+        new BukkitRunnable() {
+            public void run() {
+                Bukkit.getConsoleSender().sendMessage("[UntilTheEnd]插件检查更新中......");
+                Bukkit.getConsoleSender().sendMessage("[UntilTheEnd]插件当前版本为>>" + getDescription().getVersion());
+                latestVersion = getLatestVersion();
+                if (latestVersion == null) {
+                    return;
+                }
+                if (latestVersion.equalsIgnoreCase(getDescription().getVersion())) {
+                    getLogger().info("您的插件已经是最新版啦！");
+                } else {
+                    getLogger().info("您的插件不是最新版，请立即更新！");
+                    isLatest = false;
+                    Bukkit.getOnlinePlayers().forEach(this::sendUpdate);
+                    Bukkit.getPluginManager().registerEvents(new Listener() {
+                        @EventHandler
+                        public void onPlayerJoin(PlayerJoinEvent event) {
+                            sendUpdate(event.getPlayer());
+                        }
+                    }, UntilTheEnd.this);
+                }
+            }
+
+            private void sendUpdate(Player player) {
+                if (player.hasPermission("ute.update"))
+                    player.sendMessage("§6§l[UntilTheEnd]§4§l您的插件不是最新版，很有可能有BUG！");
+            }
+        }.runTaskAsynchronously(this);
+    }
+
+    public static String getLatestVersion() {
+        HttpURLConnection connection = null;
+        try {
+            int timeout = 5000;
+            URL url = new URL("https://untiltheend.coding.net/p/UntilTheEnd/d/UntilTheEnd/git/raw/master/UTEversion.txt");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(timeout);
+            InputStream inStream = connection.getInputStream();
+            final StringBuilder builder = new StringBuilder(255);
+            int byteRead;
+            while ((byteRead = inStream.read()) != -1) {
+                builder.append((char) byteRead);
+            }
+            return builder.toString();
+        } catch (Exception exception) {
+            INSTANCE.getLogger().log(Level.WARNING, "Failed to get update info.", exception);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return null; //这个return代表获取不到版本时返回的字符串，当然你可以改别的
+    }
 }
