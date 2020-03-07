9366d5a66e57f22faf7630bcbfd1926df586fc50
Karlatemp <karlatemp@vip.qq.com>
2020-03-08 01:54:56 +0800

    Biome Checking

--- src/HamsterYDS/UntilTheEnd/cap/hum/ChangeTasks.java
+++ src/HamsterYDS/UntilTheEnd/cap/hum/ChangeTasks.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -15,74 +16,84 @@ import HamsterYDS.UntilTheEnd.UntilTheEnd;
 import HamsterYDS.UntilTheEnd.player.PlayerManager;
 
 public class ChangeTasks {
-	public static UntilTheEnd plugin;
-	public static ArrayList<String> umbrellas=new ArrayList<String>();
-	public static ArrayList<String> waterProofSuits=new ArrayList<String>();
-	public static long weahterChangePeriod=Humidity.yaml.getLong("weahterChangePeriod");
-	public static long stateChangePeriod=Humidity.yaml.getLong("stateChangePeriod");
-	public ChangeTasks(UntilTheEnd plugin) {
-		this.plugin=plugin;
-		new WeatherTask().runTaskTimer(plugin,0L,weahterChangePeriod);
-		new StateTask().runTaskTimer(plugin,0L,stateChangePeriod);
-	}
-	public class WeatherTask extends BukkitRunnable{
-		@Override
-		public void run() {
-			for(World world:Config.enableWorlds) {
-				if(world.hasStorm()) {
-					for(Player player:world.getPlayers()) {
-						PlayerInventory inv=player.getInventory();
-						ItemStack rightHand=inv.getItemInMainHand();
-						ItemStack leftHand=inv.getItemInOffHand();
-						if(isUmbrella(rightHand)||isUmbrella(leftHand)) continue;
-						ItemStack[] armors=inv.getArmorContents();
-						boolean hasSuit=false;
-						for(ItemStack armor:armors) {
-							if(isSuit(armor)) hasSuit=true;
-						}
-						if(hasSuit) continue;
-						if(hasShelter(player)) continue;
-						PlayerManager.change(player,"hum",1);
-					}
-				}else {
-					for(Player player:world.getPlayers()) 
-						PlayerManager.change(player,"hum",-1);
-				}
-			}
-		}
-		public boolean isUmbrella(ItemStack item) {
-			if(item==null) return false;
-			if(item.hasItemMeta())
-				if(item.getItemMeta().hasDisplayName())
-					if(umbrellas.contains(item.getItemMeta().getDisplayName()))
-						return true;
-			return false;
-		}
-		public boolean isSuit(ItemStack item) {
-			if(item==null) return false;
-			if(item.hasItemMeta())
-				if(item.getItemMeta().hasDisplayName())
-					if(waterProofSuits.contains(item.getItemMeta().getDisplayName()))
-						return true;
-			return false;
-		}
-		public boolean hasShelter(Player player) {
-			Location loc=player.getLocation();
-			for(int i=0;i<=100;loc=loc.add(0,1.0,0),i++) 
-				if(player.getWorld().getBlockAt(loc).getType()!=Material.AIR) 
-					return true;
-			return false;
-		}
-	}
-	public class StateTask extends BukkitRunnable{
-		@Override
-		public void run() {
-			for(World world:Config.enableWorlds)
-				for(Player player:world.getPlayers()) {
-					Location loc=player.getLocation();
-					if(world.getBlockAt(loc).getType().equals(Material.WATER)||world.getBlockAt(loc).getType().equals(Material.STATIONARY_WATER)) 
-						PlayerManager.change(player,"hum",1);
-				}
-		}
-	}
+    public static UntilTheEnd plugin;
+    public static ArrayList<String> umbrellas = new ArrayList<String>();
+    public static ArrayList<String> waterProofSuits = new ArrayList<String>();
+    public static long weahterChangePeriod = Humidity.yaml.getLong("weahterChangePeriod");
+    public static long stateChangePeriod = Humidity.yaml.getLong("stateChangePeriod");
+
+    public ChangeTasks(UntilTheEnd plugin) {
+        ChangeTasks.plugin = plugin;
+        new WeatherTask().runTaskTimer(plugin, 0L, weahterChangePeriod);
+        new StateTask().runTaskTimer(plugin, 0L, stateChangePeriod);
+    }
+
+    public static class WeatherTask extends BukkitRunnable {
+        @Override
+        public void run() {
+            for (World world : Config.enableWorlds) {
+                if (world.hasStorm()) {
+                    for (Player player : world.getPlayers()) {
+                    	// 莫得雨
+                        if (player.getLocation().getBlock().getTemperature() > 1.0) {
+                            continue;
+                        }
+                        PlayerInventory inv = player.getInventory();
+                        ItemStack rightHand = inv.getItemInMainHand();
+                        ItemStack leftHand = inv.getItemInOffHand();
+                        if (isUmbrella(rightHand) || isUmbrella(leftHand)) continue;
+                        ItemStack[] armors = inv.getArmorContents();
+                        boolean hasSuit = false;
+                        for (ItemStack armor : armors) {
+                            if (isSuit(armor)) hasSuit = true;
+                        }
+                        if (hasSuit) continue;
+                        if (hasShelter(player)) continue;
+                        PlayerManager.change(player, "hum", 1);
+                    }
+                } else {
+                    for (Player player : world.getPlayers())
+                        PlayerManager.change(player, "hum", -1);
+                }
+            }
+        }
+
+        public boolean isUmbrella(ItemStack item) {
+            if (item == null) return false;
+            if (item.hasItemMeta())
+                if (item.getItemMeta().hasDisplayName())
+                    if (umbrellas.contains(item.getItemMeta().getDisplayName()))
+                        return true;
+            return false;
+        }
+
+        public boolean isSuit(ItemStack item) {
+            if (item == null) return false;
+            if (item.hasItemMeta())
+                if (item.getItemMeta().hasDisplayName())
+                    if (waterProofSuits.contains(item.getItemMeta().getDisplayName()))
+                        return true;
+            return false;
+        }
+
+        public boolean hasShelter(Player player) {
+            Location loc = player.getLocation();
+            for (int i = 0; i <= 100; loc = loc.add(0, 1.0, 0), i++)
+                if (player.getWorld().getBlockAt(loc).getType() != Material.AIR)
+                    return true;
+            return false;
+        }
+    }
+
+    public static class StateTask extends BukkitRunnable {
+        @Override
+        public void run() {
+            for (World world : Config.enableWorlds)
+                for (Player player : world.getPlayers()) {
+                    Location loc = player.getLocation();
+                    if (world.getBlockAt(loc).getType().equals(Material.WATER) || world.getBlockAt(loc).getType().equals(Material.STATIONARY_WATER))
+                        PlayerManager.change(player, "hum", 1);
+                }
+        }
+    }
 }
--- src/HamsterYDS/UntilTheEnd/cap/tem/Temperature.java
+++ src/HamsterYDS/UntilTheEnd/cap/tem/Temperature.java
@@ -12,16 +12,16 @@ import HamsterYDS.UntilTheEnd.UntilTheEnd;
  * @version V5.1.1
  */
 public class Temperature {
-	public static UntilTheEnd plugin; 
-	public static YamlConfiguration yaml;
-	public Temperature(UntilTheEnd plugin) {
-		this.plugin=plugin;
-		Config.autoUpdateConfigs("temperature.yml");
-		File file=new File(plugin.getDataFolder(),"temperature.yml");
-		yaml=YamlConfiguration.loadConfiguration(file);
-		System.out.println("[UntilTheEnd]正在加载温度计算模块......");
-		new TemperatureProvider(plugin);
-		new ChangeTasks(plugin);
-		new InfluenceTasks(plugin);
-	}
+    public static YamlConfiguration yaml;
+
+    public Temperature(UntilTheEnd plugin) {
+        Config.autoUpdateConfigs("temperature.yml");
+        File file = new File(plugin.getDataFolder(), "temperature.yml");
+        yaml = YamlConfiguration.loadConfiguration(file);
+        System.out.println("[UntilTheEnd]正在加载温度计算模块......");
+        new TemperatureProvider(plugin);
+        new ChangeTasks(plugin);
+        new InfluenceTasks(plugin);
+        new BiomeTasks();
+    }
 }
