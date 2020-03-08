e5de620b2a5651cb02422cd139a5f71a83543d3c
XAEGZgjOBO <1294243258@qq.com>
2020-03-08 13:26:49 +0800

    恒温服投入使用  ChangeTasks.clothesChangeTemperature.put("§6恒温服",2.0);  2.0表示无论何时不会随自然环境而改变

--- src/HamsterYDS/UntilTheEnd/item/clothes/ConstantTemperatureClothes.java
+++ src/HamsterYDS/UntilTheEnd/item/clothes/ConstantTemperatureClothes.java
@@ -12,9 +12,8 @@ public class ConstantTemperatureClothes {
         HashMap<ItemStack, Integer> materials = new HashMap<ItemStack, Integer>();
         materials.put(ItemManager.namesAndItems.get("§6齿轮"), 2);
         materials.put(ItemManager.namesAndItems.get("§6电器元件"), 2);
-        materials.put(ItemManager.namesAndItems.get("§6噩梦燃料"), 2);
+        materials.put(ItemManager.namesAndItems.get("§6噩梦燃料"), 3);
         materials.put(ItemManager.namesAndItems.get("§6绳子"), 2);
-        materials.put(new ItemStack(Material.WOOL),1);
         ItemManager.registerRecipe(materials, ItemManager.namesAndItems.get("§6恒温服"), "§6衣物");
 
         ChangeTasks.clothesChangeTemperature.put("§6恒温服",2.0);
