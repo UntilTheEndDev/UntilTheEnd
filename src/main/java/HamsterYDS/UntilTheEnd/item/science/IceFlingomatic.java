90ef22a776ff110dec2f794666ec45ebb5eac7ca
南外丶仓鼠 <15717993503@163.com>
2020-05-05 07:58:56 +0800

    FIX A BUG: ICEFLINGO NPE EXCEPTION

--- src/main/java/HamsterYDS/UntilTheEnd/item/science/IceFlingomatic.java
+++ src/main/java/HamsterYDS/UntilTheEnd/item/science/IceFlingomatic.java
@@ -14,25 +14,29 @@ import HamsterYDS.UntilTheEnd.item.ItemManager;
  * @version V5.1.1
  */
 public class IceFlingomatic implements Listener {
-    public static int range = ItemManager.itemAttributes.getInt("IceFlingomatic.range");
+	public static int range = ItemManager.itemAttributes.getInt("IceFlingomatic.range");
 
-    public IceFlingomatic() {
-        ItemManager.plugin.getServer().getPluginManager().registerEvents(this, ItemManager.plugin);
-    }
+	public IceFlingomatic() {
+		ItemManager.plugin.getServer().getPluginManager().registerEvents(this, ItemManager.plugin);
+	}
 
-    @EventHandler
-    public void onBurn(BlockIgniteEvent event) {
-        if (event.isCancelled()) return;
-        if (!Config.enableWorlds.contains(event.getIgnitingBlock().getWorld())) return;
-        for (String str : BlockApi.getSpecialBlocks("IceFlingomatic")) {
-            Location loc = BlockApi.strToLoc(str);
-            Location loc2 = event.getBlock().getLocation();
-            if (loc.distance(loc2) <= range) {
-                loc2.getWorld().spawnParticle(Particle.SNOWBALL, loc2, 5);
-                loc.getWorld().spawnParticle(Particle.SNOWBALL, loc.add(0.5, 1.0, 0.5), 5);
-                event.setCancelled(true);
-                return;
-            }
-        }
-    }
+	@EventHandler
+	public void onBurn(BlockIgniteEvent event) {
+		if (event.isCancelled())
+			return;
+		if (event.getIgnitingBlock() == null)
+			return;
+		if (!Config.enableWorlds.contains(event.getIgnitingBlock().getWorld()))
+			return;
+		for (String str : BlockApi.getSpecialBlocks("IceFlingomatic")) {
+			Location loc = BlockApi.strToLoc(str);
+			Location loc2 = event.getBlock().getLocation();
+			if (loc.distance(loc2) <= range) {
+				loc2.getWorld().spawnParticle(Particle.SNOWBALL, loc2, 5);
+				loc.getWorld().spawnParticle(Particle.SNOWBALL, loc.add(0.5, 1.0, 0.5), 5);
+				event.setCancelled(true);
+				return;
+			}
+		}
+	}
 }
