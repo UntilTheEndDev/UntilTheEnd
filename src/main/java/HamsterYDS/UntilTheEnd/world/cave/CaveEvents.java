7def56560fcc5720a2e8b079c6502a6572cb3b10
Karlatemp <karlatemp@vip.qq.com>
2020-05-25 22:22:00 +0800

    make `static`

--- src/main/java/HamsterYDS/UntilTheEnd/world/cave/CaveEvents.java
+++ src/main/java/HamsterYDS/UntilTheEnd/world/cave/CaveEvents.java
@@ -1,8 +1,8 @@
 package HamsterYDS.UntilTheEnd.world.cave;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import HamsterYDS.UntilTheEnd.UntilTheEnd;
+import HamsterYDS.UntilTheEnd.api.BlockApi;
+import HamsterYDS.UntilTheEnd.internal.NPCChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -12,115 +12,117 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import HamsterYDS.UntilTheEnd.UntilTheEnd;
-import HamsterYDS.UntilTheEnd.api.BlockApi;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CaveEvents implements Listener {
-	public static boolean isEarthQuaking = false;
-	public static int lastPeriod = 0;
+    public static boolean isEarthQuaking = false;
+    public static int lastPeriod = 0;
 
-	public CaveEvents() {
-		new CaveQuakeTrigger().runTaskTimerAsynchronously(UntilTheEnd.getInstance(), 0L, 20L);
-		new RockFallingManager().runTaskTimer(UntilTheEnd.getInstance(), 0L, 30L);
-		new EyeDirectionChanger().runTaskTimer(UntilTheEnd.getInstance(), 0L, 5L);
-		Bukkit.getPluginManager().registerEvents(this, UntilTheEnd.getInstance());
-	}
+    public CaveEvents() {
+        new CaveQuakeTrigger().runTaskTimerAsynchronously(UntilTheEnd.getInstance(), 0L, 20L);
+        new RockFallingManager().runTaskTimer(UntilTheEnd.getInstance(), 0L, 30L);
+        new EyeDirectionChanger().runTaskTimer(UntilTheEnd.getInstance(), 0L, 5L);
+        Bukkit.getPluginManager().registerEvents(this, UntilTheEnd.getInstance());
+    }
 
-	class CaveQuakeTrigger extends BukkitRunnable {
+    static class CaveQuakeTrigger extends BukkitRunnable {
 
-		@Override
-		public void run() {
-			lastPeriod = lastPeriod - 1;
-			if (lastPeriod <= 0) {
-				isEarthQuaking = false;
-			}
-			if (!isEarthQuaking) {
-				if (Math.random() <= 0.01) {
-					isEarthQuaking = true;
-					lastPeriod = (int) (Math.random() * 120);
-				}
-			}
-		}
+        @Override
+        public void run() {
+            lastPeriod = lastPeriod - 1;
+            if (lastPeriod <= 0) {
+                isEarthQuaking = false;
+            }
+            if (!isEarthQuaking) {
+                if (Math.random() <= 0.01) {
+                    isEarthQuaking = true;
+                    lastPeriod = (int) (Math.random() * 120);
+                }
+            }
+        }
 
-	}
+    }
 
-	// 视角摇晃
-	class EyeDirectionChanger extends BukkitRunnable {
+    // 视角摇晃
+    static class EyeDirectionChanger extends BukkitRunnable {
 
-		@Override
-		public void run() {
-			if (!isEarthQuaking)
-				return;
+        @Override
+        public void run() {
+            if (!isEarthQuaking)
+                return;
 
-			for (Player player : CaveManager.cave.getPlayers()) {
-				Location loc = player.getLocation();
-				if (Math.random() <= 0.02) {
-					loc.setYaw((float) (loc.getYaw() + Math.random() * 3 - Math.random() * 3));
-					loc.setPitch((float) (loc.getPitch() + Math.random() * 3 - Math.random() * 3));
-					player.teleport(loc);
-				}
+            for (Player player : CaveManager.cave.getPlayers()) {
+                Location loc = player.getLocation();
+                if (Math.random() <= 0.02) {
+                    loc.setYaw((float) (loc.getYaw() + Math.random() * 3 - Math.random() * 3));
+                    loc.setPitch((float) (loc.getPitch() + Math.random() * 3 - Math.random() * 3));
+                    player.teleport(loc);
+                }
 
-				if (Math.random() <= 0.2)
-					player.setVelocity(loc.subtract(loc.clone().add(0, 5, 0)).toVector());
-				if (Math.random() <= 0.2)
-					player.setVelocity(loc.subtract(loc.clone().add(0, -5, 0)).toVector());
-			}
-		}
-	}
+                if (Math.random() <= 0.2)
+                    player.setVelocity(loc.subtract(loc.clone().add(0, 5, 0)).toVector());
+                if (Math.random() <= 0.2)
+                    player.setVelocity(loc.subtract(loc.clone().add(0, -5, 0)).toVector());
+            }
+        }
+    }
 
-	// 石块掉落
-	class RockFallingManager extends BukkitRunnable {
+    // 石块掉落
+    static class RockFallingManager extends BukkitRunnable {
 
-		@Override
-		public void run() {
-			if (!isEarthQuaking)
-				return;
-			for (Player player : CaveManager.cave.getPlayers()) {
-				Location loc = player.getLocation();
-				Location fallingLoc = loc.clone();
-				for (int i = 0; i <= 100; i++) {
-					if (Math.random() <= 0.2) {
-						fallingLoc.add(Math.random() * 64 - Math.random() * 64, 0,
-								Math.random() * 64 - Math.random() * 64);
-						List<String> hasSupportLocs = new ArrayList<String>();
-						boolean tmp = false;
-						for (int y = 3; y <= 62; y++) {
-							fallingLoc.setY(y);
-							if (tmp) {
-								hasSupportLocs.add(BlockApi.locToStr(fallingLoc));
-								continue;
-							}
-							if (fallingLoc.getBlock().getType() == Material.AIR) {
-								y--;
-								tmp = true;
-							}
-						}
-						for (String toString : hasSupportLocs) {
-							Location collapsePos = BlockApi.strToLoc(toString);
-							Block block = collapsePos.getBlock();
-							FallingBlock entity = CaveManager.cave.spawnFallingBlock(collapsePos, block.getType(),
-									block.getData());
-							entity.setHurtEntities(true);
-							entity.setDropItem(false);
-							block.setType(Material.AIR);
-							new BukkitRunnable() {
+        @Override
+        public void run() {
+            if (!isEarthQuaking)
+                return;
+            for (Player player : CaveManager.cave.getPlayers()) {
+                if (NPCChecker.isNPC(player)) continue;
+                Location loc = player.getLocation();
+                Location fallingLoc = loc.clone();
+                for (int i = 0; i <= 100; i++) {
+                    if (Math.random() <= 0.2) {
+                        fallingLoc.add(Math.random() * 64 - Math.random() * 64, 0,
+                                Math.random() * 64 - Math.random() * 64);
+                        List<String> hasSupportLocs = new ArrayList<>();
+                        boolean tmp = false;
+                        for (int y = 3; y <= 62; y++) {
+                            fallingLoc.setY(y);
+                            if (tmp) {
+                                hasSupportLocs.add(BlockApi.locToStr(fallingLoc));
+                                continue;
+                            }
+                            if (fallingLoc.getBlock().getType() == Material.AIR) {
+                                y--;
+                                tmp = true;
+                            }
+                        }
+                        for (String toString : hasSupportLocs) {
+                            Location collapsePos = BlockApi.strToLoc(toString);
+                            assert collapsePos != null;
+                            Block block = collapsePos.getBlock();
+                            FallingBlock entity = CaveManager.cave.spawnFallingBlock(collapsePos, block.getType(),
+                                    block.getData());
+                            entity.setHurtEntities(true);
+                            entity.setDropItem(false);
+                            block.setType(Material.AIR);
+                            new BukkitRunnable() {
 
-								@Override
-								public void run() {
-									entity.setVelocity(collapsePos.clone().add(Math.random(), 0, Math.random())
-											.subtract(collapsePos).toVector());
-									if (entity.isOnGround())
-										cancel();
-								}
+                                @Override
+                                public void run() {
+                                    entity.setVelocity(collapsePos.clone().add(Math.random(), 0, Math.random())
+                                            .subtract(collapsePos).toVector());
+                                    if (entity.isOnGround())
+                                        cancel();
+                                }
 
-							}.runTaskTimer(UntilTheEnd.getInstance(), 0L, 20L);
+                            }.runTaskTimer(UntilTheEnd.getInstance(), 0L, 20L);
 
-						}
-						fallingLoc = loc.clone();
-					}
-				}
-			}
-		}
+                        }
+                        fallingLoc = loc.clone();
+                    }
+                }
+            }
+        }
 
-	}
+    }
 }
