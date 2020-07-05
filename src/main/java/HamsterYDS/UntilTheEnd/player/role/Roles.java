140d559985f3338db68e33372b011719d499f989
Karlatemp <karlatemp@vip.qq.com>
2020-07-05 14:29:30 +0800

    Fix logger

--- src/main/java/HamsterYDS/UntilTheEnd/player/role/Roles.java
+++ src/main/java/HamsterYDS/UntilTheEnd/player/role/Roles.java
@@ -1,6 +1,7 @@
 package HamsterYDS.UntilTheEnd.player.role;
 
 import HamsterYDS.UntilTheEnd.Config;
+import HamsterYDS.UntilTheEnd.Logging;
 import HamsterYDS.UntilTheEnd.UntilTheEnd;
 import HamsterYDS.UntilTheEnd.internal.DisableManager;
 import org.bukkit.configuration.ConfigurationSection;
@@ -40,31 +41,31 @@ public enum Roles {
 
     static {
         final UntilTheEnd instance = UntilTheEnd.getInstance();
-        instance.getLogger().fine("[Role] Loading rule configuration......");
+        Logging.getLogger().fine("[Role] Loading rule configuration......");
         final YamlConfiguration configs = Config.autoUpdateConfigs("roles.yml");
         isEnable = configs.getBoolean("enabled", true);
-        instance.getLogger().fine("[Role] Was Role enabled? > " + isEnable);
+        Logging.getLogger().fine("[Role] Was Role enabled? > " + isEnable);
         final Set<String> roles = DisableManager.root.getStringList("roles").stream().map(String::toLowerCase).collect(Collectors.toSet());
         for (Roles r : values()) {
-            instance.getLogger().fine("[Role] Loading settings for " + r.name());
+            Logging.getLogger().fine("[Role] Loading settings for " + r.name());
             if (roles.contains(r.name())) {
                 instance.getLogger().fine("[Role] \tRole [" + r.name() + "] was disabled in disable.yml....");
                 r.allow = false;
             }
             final ConfigurationSection section = configs.getConfigurationSection(r.name().toLowerCase());
             if (section == null) {
-                instance.getLogger().fine("[Role] \tSettings for " + r.name() + " not found. use default values.");
+                Logging.getLogger().fine("[Role] \tSettings for " + r.name() + " not found. use default values.");
             } else {
                 r.name = section.getString("name", r.name);
                 r.originLevel = section.getInt("originLevel", r.originLevel);
                 r.originSanMax = section.getInt("originSanMax", r.originSanMax);
                 r.originHealthMax = section.getInt("originHealthMax", r.originHealthMax);
                 r.originDamageLevel = section.getDouble("originDamageLevel", r.originDamageLevel);
-                instance.getLogger().fine("[Role] \t\tName     : " + r.name);
-                instance.getLogger().fine("[Role] \t\tLevel    : " + r.originLevel);
-                instance.getLogger().fine("[Role] \t\tMaxHealth: " + r.originHealthMax);
-                instance.getLogger().fine("[Role] \t\tMaxSanity: " + r.originSanMax);
-                instance.getLogger().fine("[Role] \t\tDamageLv : " + r.originDamageLevel);
+                Logging.getLogger().fine("[Role] \t\tName     : " + r.name);
+                Logging.getLogger().fine("[Role] \t\tLevel    : " + r.originLevel);
+                Logging.getLogger().fine("[Role] \t\tMaxHealth: " + r.originHealthMax);
+                Logging.getLogger().fine("[Role] \t\tMaxSanity: " + r.originSanMax);
+                Logging.getLogger().fine("[Role] \t\tDamageLv : " + r.originDamageLevel);
             }
         }
         RolesSettings.initialize(configs);
