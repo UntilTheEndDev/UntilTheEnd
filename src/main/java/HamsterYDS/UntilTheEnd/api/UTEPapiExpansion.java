87830ac24faa6386853bfeb6a6172e8bd9129dfe
Karlatemp <karlatemp@vip.qq.com>
2020-07-12 18:12:52 +0800

    PAPI: if-else -> switch

--- src/main/java/HamsterYDS/UntilTheEnd/api/UTEPapiExpansion.java
+++ src/main/java/HamsterYDS/UntilTheEnd/api/UTEPapiExpansion.java
@@ -28,56 +28,58 @@ public class UTEPapiExpansion extends PlaceholderExpansion {
     public String onPlaceholderRequest(Player player, String identifier) {
         if (player == null) {
             return "";
-        } else if (identifier.equals("san")) {
-            return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.SANITY)));
-        } else if (identifier.equals("tem")) {
-            return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.TEMPERATURE)));
-        } else if (identifier.equals("hum")) {
-            return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.HUMIDITY)));
-        } else if (identifier.equals("tir")) {
-            return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.TIREDNESS)));
-        } else if (identifier.equals("season")) {
-            return WorldApi.getName(WorldApi.getSeason(player.getWorld()));
-        } else if (identifier.equals("role")) {
-            return PlayerApi.getRole(player).name;
-        } else if (identifier.equals("sanmax")) {
-            return String.valueOf(PlayerManager.check(player, CheckType.SANMAX));
-        } else if (identifier.equals("healthmax")) {
-            return String.valueOf(PlayerManager.check(player, CheckType.HEALTHMAX));
-        } else if (identifier.equals("level")) {
-            return String.valueOf(PlayerManager.check(player, CheckType.LEVEL));
-        } else if (identifier.equals("damagelevel")) {
-            return String.valueOf(PlayerManager.check(player, CheckType.DAMAGELEVEL));
-        } else if (identifier.equals("day")) {
-            return String.valueOf(WorldApi.getDay(player.getWorld()));
-        } else if (identifier.equals("sanitycolor")) {
-            return String.valueOf(PlayerApi.getSanityColor(player));
-        } else if (identifier.equals("temperaturecolor")) {
-            return String.valueOf(PlayerApi.getTemperatureColor(player));
-        } else if (identifier.equals("humiditycolor")) {
-            return String.valueOf(PlayerApi.getHumidityColor(player));
-        } else if (identifier.equals("tirednesscolor")) {
-            return String.valueOf(PlayerApi.getTirednessColor(player));
-        } else if (identifier.equals("seasoncolor")) {
-            return String.valueOf(WorldApi.getSeasonColor(player.getWorld()));
-        } else if (identifier.equals("sanitytend")) {
-            return String.valueOf(PlayerApi.getChangingTend(player, "san"));
-        } else if (identifier.equals("temperaturetend")) {
-            return String.valueOf(PlayerApi.getChangingTend(player, "tem"));
-        } else if (identifier.equals("humiditytend")) {
-            return String.valueOf(PlayerApi.getChangingTend(player, "hum"));
-        } else if (identifier.equals("tirednesstend")) {
-            return String.valueOf(PlayerApi.getChangingTend(player, "tir"));
-        } else if (identifier.equals("sanitybar")) {
-            return PlayerApi.getSanityBar(player);
-        } else if (identifier.equals("humiditybar")) {
-            return PlayerApi.getHumidityBar(player);
-        } else if (identifier.equals("temperaturebar")) {
-            return PlayerApi.getTemperatureBar(player);
-        } else if (identifier.equals("tirednessbar")) {
-            return PlayerApi.getTirednessBar(player);
-        } else {
-            return identifier;
+        }
+        switch (identifier) {
+            case "san":
+                return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.SANITY)));
+            case "tem":
+                return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.TEMPERATURE)));
+            case "hum":
+                return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.HUMIDITY)));
+            case "tir":
+                return String.valueOf(MathHelper.p2(PlayerManager.check(player, CheckType.TIREDNESS)));
+            case "season":
+                return WorldApi.getName(WorldApi.getSeason(player.getWorld()));
+            case "role":
+                return PlayerApi.getRole(player).name;
+            case "sanmax":
+                return String.valueOf(PlayerManager.check(player, CheckType.SANMAX));
+            case "healthmax":
+                return String.valueOf(PlayerManager.check(player, CheckType.HEALTHMAX));
+            case "level":
+                return String.valueOf(PlayerManager.check(player, CheckType.LEVEL));
+            case "damagelevel":
+                return String.valueOf(PlayerManager.check(player, CheckType.DAMAGELEVEL));
+            case "day":
+                return String.valueOf(WorldApi.getDay(player.getWorld()));
+            case "sanitycolor":
+                return String.valueOf(PlayerApi.getSanityColor(player));
+            case "temperaturecolor":
+                return String.valueOf(PlayerApi.getTemperatureColor(player));
+            case "humiditycolor":
+                return String.valueOf(PlayerApi.getHumidityColor(player));
+            case "tirednesscolor":
+                return String.valueOf(PlayerApi.getTirednessColor(player));
+            case "seasoncolor":
+                return String.valueOf(WorldApi.getSeasonColor(player.getWorld()));
+            case "sanitytend":
+                return String.valueOf(PlayerApi.getChangingTend(player, "san"));
+            case "temperaturetend":
+                return String.valueOf(PlayerApi.getChangingTend(player, "tem"));
+            case "humiditytend":
+                return String.valueOf(PlayerApi.getChangingTend(player, "hum"));
+            case "tirednesstend":
+                return String.valueOf(PlayerApi.getChangingTend(player, "tir"));
+            case "sanitybar":
+                return PlayerApi.getSanityBar(player);
+            case "humiditybar":
+                return PlayerApi.getHumidityBar(player);
+            case "temperaturebar":
+                return PlayerApi.getTemperatureBar(player);
+            case "tirednessbar":
+                return PlayerApi.getTirednessBar(player);
+            default:
+                return identifier;
         }
     }
 }
