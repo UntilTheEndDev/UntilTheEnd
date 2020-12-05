56bb4d55498ce79baedb42196a0781a4fa3d739b
HamsterYDS <15717993503@163.com>
2020-12-05 15:49:13 +0800

    1.16.3&1.16.4兼容

---
+++ src/main/java/ute/nms/v1_16_R4/ActionBarManagerImpl.java
@@ -0,0 +1,21 @@
+package ute.nms.v1_16_R4;
+
+import net.minecraft.server.v1_16_R2.ChatComponentText;
+import net.minecraft.server.v1_16_R2.ChatMessageType;
+import net.minecraft.server.v1_16_R2.EntityPlayer;
+import net.minecraft.server.v1_16_R2.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import ute.nms.ActionBarManager;
+
+import java.util.UUID;
+
+public class ActionBarManagerImpl extends ActionBarManager {
+    private static final UUID ZERO = new UUID(0, 0);
+
+    @Override
+    protected void sendActionBar0(Player player, String line) {
+        EntityPlayer player1 = ((CraftPlayer) player).getHandle();
+        player1.playerConnection.sendPacket(new PacketPlayOutChat(new ChatComponentText(line), ChatMessageType.GAME_INFO, ZERO));
+    }
+}
